f3377457d3d38cbd818736ece1f390569fcbe798
Miller2406 <140726735+Miller2406@users.noreply.github.com>
2023-11-07 16:28:58 +0700

    update

---
+++ index.js
@@ -0,0 +1,8 @@
+// Challenge: Pickles Exercise
+// คุณได้รับ index.html
+
+// โปรดใช้ JavaScript เพื่อเลือกองค์ประกอบ "Delicious"
+// เปลี่ยนข้อความเป็น "Disgusting" โดยใช้ JavaScript
+// ใช่ คุณสามารถโกงและอัปเดตไฟล์ html ได้โดยตรง แต่หวังคุณจะไม่ทำ! เป้าหมายที่นี่คือการฝึกใช้ JavaScript เพื่อจัดการกับ HTML
+
+document.querySelector('span').innerText = 'Disgusting'
